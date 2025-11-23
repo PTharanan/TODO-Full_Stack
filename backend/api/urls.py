68403449a25437ce332196b12175a0bf3ace46fc
ASUS <ptharanan@gmail.com>
2025-11-23 20:05:34 +0530

    Add initial project configuration files for Python module setup

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import Data_API
 
 urlpatterns = [
-    path('task/', Data_API.as_view())
+    path('task/', Data_API.as_view()),
+    path('task/<int:task_id>/', Data_API.as_view()),
 ]
